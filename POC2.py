2a1103ef2ada48c546a36923707092f65cd7ec12
GuillaumeVandeMaele <63400974+GuillaumeVandeMaele@users.noreply.github.com>
2020-06-08 18:43:11 +0200

    Update POC2.py

--- POC2.py
+++ POC2.py
@@ -6,9 +6,9 @@ import cv2
 from tqdm import tqdm
 
 # Set options like file name,...
-file_name = "FridaKahlo"  # Filename of the bigpicture
+file_name = "DemonSlayer"  # Filename of the bigpicture
 NUMBER_PIXPICS = 2000     # Amount of small images that will be used to create the big picture
-MAX_IM = 10               # Maximum amount of images that will be iterated over to try and recreate the bigpicture
+MAX_IM = 10             # Maximum amount of images that will be iterated over to try and recreate the bigpicture
 RESOLUTION_FACTOR = 2     # The upscaling factor to increase the size of the final image 
 MOST_USED_THRESHOLD = 0.12 # Determines a treshold to show the most used images
 
@@ -19,8 +19,8 @@ pixel_pics_dir = parentdir + "PixelPictures5//"
 save_dir = parentdir + "Finished//"
 
 # Read in the big picture and identify the needed parameters
-MainPicture = plt.imread(main_pic_dir)
-height1, width1 = len(MainPicture), len(MainPicture[0])
+mainpicture = plt.imread(main_pic_dir)
+height1, width1 = len(mainpicture), len(mainpicture[0])
 
 # Determine size of pixel pictures
 dim_pix = int(np.sqrt((height1*width1)/NUMBER_PIXPICS))
@@ -30,7 +30,7 @@ amount_widht = width1//dim_pix
 # Resize the main picture to fit perfect squares of dim_pix init
 diff_height = height1-dim_pix*amount_height
 diff_width = width1-dim_pix*amount_widht
-MainPicture = MainPicture[diff_height//2:dim_pix*amount_height+diff_height//2, diff_width//2 :dim_pix*amount_widht+diff_width//2] #TODO: kortere lijn door op volgende 'lijn' te zetten
+mainpicture = mainpicture[diff_height//2:dim_pix*amount_height+diff_height//2, diff_width//2 :dim_pix*amount_widht+diff_width//2] #TODO: kortere lijn door op volgende 'lijn' te zetten
 
 # Create 'empty' image for recontstruction
 reconstructed = []
@@ -57,8 +57,10 @@ for h in range(amount_height):
 current_error = np.array(current_error)
 image_count = np.array(image_count)
 
-# Read in the pixel pictures, resize, calculate mean and associate with correct spot
-for im_num in tqdm(range(1, MAX_IM+1)): #gebruik een for lus
+# Read in the pixel pictures, resize, calculate error and associate with correct spot
+for im_num in tqdm(range(1, MAX_IM+1)):
+
+    # Read the small image in
     pixel_pic =  plt.imread(pixel_pics_dir + "Pixel_Im ("+ str(im_num) + ").jpg")  #TODO: zie tweede tutorial van manu
 
     # Cut the image to correct ratio (square)
@@ -73,12 +75,12 @@ for im_num in tqdm(range(1, MAX_IM+1)): #gebruik een for lus
     # Resize the image to dim_pix by dim_pix 
     pixel_pic = cv2.resize(pixel_pic, (dim_pix,dim_pix), interpolation = cv2.INTER_AREA)
 
-    # Calculate error and reconstruct the image
+    # Calculate error based on the sum of the euclidian distances between rgb values of the main picture and the "pixel_pic"
     error = []
     for h in range(amount_height):
         error2 = []
         for w in range(amount_widht):
-            pixel_diff = np.array(pixel_pic-MainPicture[h*dim_pix:(h+1)*dim_pix, w*dim_pix:(w+1)*dim_pix])
+            pixel_diff = np.array(pixel_pic-mainpicture[h*dim_pix:(h+1)*dim_pix, w*dim_pix:(w+1)*dim_pix])
             error1 = []
             for pain in pixel_diff:
                 error1.append(np.sum(np.sqrt(np.sum(np.array(pain**2), axis = 1))))
