016e2e62b0f4668d0de132e7b97b93f3222a3e34
GuillaumeVandeMaele <63400974+GuillaumeVandeMaele@users.noreply.github.com>
2020-06-09 10:04:08 +0200

    Update POC2_modular.py

--- POC2_modular.py
+++ POC2_modular.py
@@ -5,9 +5,16 @@ import PIL
 import cv2
 from tqdm import tqdm
 
+# Define some constants 
+file_name = "DemonSlayer"  # Filename of the bigpicture
+NUMBER_PIXPICS = 2000     # Amount of small images that will be used to create the big picture
+MAX_IM = 10            # Maximum amount of images that will be iterated over to try and recreate the bigpicture
+RESOLUTION_FACTOR = 2     # The upscaling factor to increase the size of the final image 
+MOST_USED_THRESHOLD = 0.12 # Determines a treshold to show the most used images
+
 
 # Main function of the program
-def Main(file_name, NUMBER_PIXPICS, MAX_IM, RESOLUTION_FACTOR, MOST_USED_THRESHOLD):
+def main(file_name, NUMBER_PIXPICS, MAX_IM, RESOLUTION_FACTOR, MOST_USED_THRESHOLD):
 
     # Read in the main picture and get needed parameters
     parentdir = "D://DocumentenDschijf//3de bach//PicturestoCollage//"
@@ -15,20 +22,20 @@ def Main(file_name, NUMBER_PIXPICS, MAX_IM, RESOLUTION_FACTOR, MOST_USED_THRESHO
     pixel_pics_dir = parentdir + "PixelPictures5//"
     save_dir = parentdir + "Finished//"
 
-    mainpicture = ReadPicture(main_pic_dir)
+    mainpicture = read_picture(main_pic_dir)
     height1, width1 = len(mainpicture), len(mainpicture[0])
 
     # Determine size of pixel pictures
-    dim_pix, amount_height, amount_width = SizePixelPic(height1, width1, NUMBER_PIXPICS)
+    dim_pix, amount_height, amount_width = size_pixel_pic(height1, width1)
     
     # Crop the main picture to fit perfect squares of dim_pix init
-    main_pic_dir = Crop(mainpicture, height1, width1, amount_height, amount_width, dim_pix)
+    main_pic_dir = crop(mainpicture, height1, width1, amount_height, amount_width, dim_pix)
 
     # Create empty image for signal reconstruction
-    reconstructed = Matrix(amount_height * dim_pix, amount_width * dim_pix, [np.uint8(0), np.uint8(0), np.uint8(0)])
+    reconstructed = create_matrix(amount_height * dim_pix, amount_width * dim_pix, [np.uint8(0), np.uint8(0), np.uint8(0)])
 
     # Create Current error matrix
-    current_error = Matrix(amount_height, amount_width, None)
+    current_error = create_matrix(amount_height, amount_width, None)
 
     # Create image counter
     image_count = current_error
@@ -37,7 +44,7 @@ def Main(file_name, NUMBER_PIXPICS, MAX_IM, RESOLUTION_FACTOR, MOST_USED_THRESHO
     for im_num in tqdm(range(1, MAX_IM+1)):
 
         # Read in the small image 
-        pixel_pic = ReadPicture(pixel_pics_dir + "Pixel_Im ("+ str(im_num) + ").jpg")
+        pixel_pic = read_picture(pixel_pics_dir + "Pixel_Im ("+ str(im_num) + ").jpg")
 
         # Cut the image to correct ratio (square)
         height2, width2 = len(pixel_pic), len(pixel_pic[0])
@@ -49,19 +56,19 @@ def Main(file_name, NUMBER_PIXPICS, MAX_IM, RESOLUTION_FACTOR, MOST_USED_THRESHO
             pixel_pic = pixel_pic[:,diff//2:width2-diff//2]
         
         # Resize the image to dim_pix by dim_pix 
-        pixel_pic = Resize(pixel_pic, dim_pix)
+        pixel_pic = resize(pixel_pic, dim_pix)
 
         # Calculate error based on the sum of the euclidian distances between rgb values of the main picture and the "pixel_pic"
-        error = Error(mainpicture, pixel_pic, amount_height, amount_width, dim_pix)
+        error = calculate_error(mainpicture, pixel_pic, amount_height, amount_width, dim_pix)
 
         # Reconstruct the image, and update image counter and current error
-        reconstructed, current_error, image_count = Reconstruct(amount_height, amount_width, error, current_error, dim_pix, reconstructed, image_count, pixel_pic, im_num)
+        reconstructed, current_error, image_count = reconstruct(amount_height, amount_width, error, current_error, dim_pix, reconstructed, image_count, pixel_pic, im_num)
 
     # Quantise the images that are not used
-    zero_images, counter, x_axis = ZeroImages(image_count, MAX_IM)
+    zero_images, counter, x_axis = create_zero_images(image_count)
     
     # Quantise the imagess that are used a lot
-    max_images = MaxImages(counter, x_axis, MOST_USED_THRESHOLD)
+    max_images = create_max_images(counter, x_axis)
 
     # Print out some of the results about used images and different amount of images
     print("Images that are not used = ", zero_images)
@@ -73,27 +80,31 @@ def Main(file_name, NUMBER_PIXPICS, MAX_IM, RESOLUTION_FACTOR, MOST_USED_THRESHO
 
     # Save the new image
     save_name = save_dir + file_name + "_collage_AmountOfImages_" + str(NUMBER_PIXPICS) + "DifferentImages_" + str(MAX_IM - len(zero_images)) + "Total_Error_" + str(tot_error) + ".jpg"
-    Save(save_name, reconstructed)
+    save(save_name, reconstructed)
 
     # Visualise the current error
-    PlotMatrix(current_error)
+    plot_matrix(current_error)
+
 
 # Function that will read in the correct picture from the correct location
-def ReadPicture(dir):
+def read_picture(dir):
     return plt.imread(dir)
 
+
 # Function that will crop the picture:
-def Resize(picture, dim_pix):
+def resize(picture, dim_pix):
     return cv2.resize(picture, (dim_pix,dim_pix), interpolation = cv2.INTER_AREA)
 
+
 # Function that will resize the picture:
-def Crop(picture, height, width, amount_height, amount_width, dim_pix):
+def crop(picture, height, width, amount_height, amount_width, dim_pix):
     diff_height = height-dim_pix*amount_height
     diff_width = width-dim_pix*amount_width
     return picture[diff_height//2:dim_pix*amount_height+diff_height//2, diff_width//2 :dim_pix*amount_width+diff_width//2]
 
+
 # Function that will calculate the error: (sum of the euclidian distances between rgb values of the main picture and the "pixel_pic")
-def Error(picture1, picture2, height, width, dim_pix):
+def calculate_error(picture1, picture2, height, width, dim_pix):
     error = []
     for h in range(height):
         error2 = []
@@ -106,8 +117,9 @@ def Error(picture1, picture2, height, width, dim_pix):
         error.append(error2)
     return np.array(error)
 
+
 # Function that will create matrices of given size and with given but fixed values init
-def Matrix(height, width, values):
+def create_matrix(height, width, values):
     matrix = []
 
     for h in range(height):
@@ -118,15 +130,17 @@ def Matrix(height, width, values):
 
     return np.array(matrix) #TODO: dubbel werk maar het gwn direct aan als np array en gebruik dan
 
+
 # Function that determines size of pixel pics
-def SizePixelPic(height, width, NUMBER_PIXPICS):
+def size_pixel_pic(height, width):
     dim_pix = int(np.sqrt((height*width)/NUMBER_PIXPICS))
     amount_height = height//dim_pix
     amount_width = width//dim_pix
     return dim_pix, amount_height, amount_width
 
+
 # Function that reconstructs the image
-def Reconstruct(height, width, error, current_error, dim_pix, reconstructed, image_count, pixel_pic, im_num):
+def reconstruct(height, width, error, current_error, dim_pix, reconstructed, image_count, pixel_pic, im_num):
     for h in range(height):
         for w in range(width):
             if current_error[h][w] == None or error[h][w] < current_error[h][w]:
@@ -136,8 +150,9 @@ def Reconstruct(height, width, error, current_error, dim_pix, reconstructed, ima
 
     return reconstructed, current_error, image_count
 
+
 # Function that looks wich images are not used
-def ZeroImages(image_count, MAX_IM):
+def create_zero_images(image_count):
     zero_images = []
     x_axis = []
     counter = []
@@ -149,8 +164,9 @@ def ZeroImages(image_count, MAX_IM):
             zero_images.append(i)
     return zero_images, counter, x_axis
 
+
 # Function that looks for images that are used a lot
-def MaxImages(counter, x_axis, MOST_USED_THRESHOLD):
+def create_max_images(counter, x_axis):
     max_images = []
 
     for j in range(len(counter)):
@@ -158,41 +174,22 @@ def MaxImages(counter, x_axis, MOST_USED_THRESHOLD):
             max_images.append(x_axis[j])
     return max_images
 
+
 # Function to save the results
-def Save(dir, data):
+def save(dir, data):
     plt.imsave(dir,data)
 
+
 # Function to visualise matrix data
-def PlotMatrix(data):
+def plot_matrix(data):
     plt.figure()
     plt.imshow(np.array(data, dtype=float))
     plt.colorbar()
     plt.show()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # Activate when this is the main program, call the main function
 if __name__ == '__main__':
-    file_name = "DemonSlayer"  # Filename of the bigpicture
-    NUMBER_PIXPICS = 2000     # Amount of small images that will be used to create the big picture
-    MAX_IM = 10            # Maximum amount of images that will be iterated over to try and recreate the bigpicture
-    RESOLUTION_FACTOR = 2     # The upscaling factor to increase the size of the final image 
-    MOST_USED_THRESHOLD = 0.12 # Determines a treshold to show the most used images
-
-    Main(file_name, NUMBER_PIXPICS, MAX_IM, RESOLUTION_FACTOR, MOST_USED_THRESHOLD)
+    main(file_name, NUMBER_PIXPICS, MAX_IM, RESOLUTION_FACTOR, MOST_USED_THRESHOLD)
 
 
